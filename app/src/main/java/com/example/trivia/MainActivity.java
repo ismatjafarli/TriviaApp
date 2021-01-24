521552b89176bdf09337fd4393a4e86c0cd45247
ismatjafarli <jafarliismat@gmail.com>
2021-01-24 16:36:41 +0400

    changes

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -36,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         questions = new Repository().getQuestions(questionArrayList -> {
             binding.questionTextView.setText(questions.get(currentQuestionNumber).getQuestion());
-                    updateCounter();
-                });
+            updateCounter();
+        });
 
         binding.nextButton.setOnClickListener(v -> {
             nextQuestion();
@@ -47,23 +51,24 @@ public class MainActivity extends AppCompatActivity {
 
         binding.trueButton.setOnClickListener(v -> {
             checkAnswer(true);
+            nextQuestion();
+
         });
 
         binding.falseButton.setOnClickListener(v -> {
             checkAnswer(false);
+            nextQuestion();
         });
-    }
-
-    private void updateCounter() {
-        binding.questionIndexText.setText("Question: " + String.valueOf(currentQuestionNumber) + "/" + String.valueOf(questions.size()));
     }
 
     private void checkAnswer(boolean answerTrue) {
         int snackBarId = 0;
         if(answerTrue == questions.get(currentQuestionNumber).isTrueAnswer()) {
             snackBarId = R.string.correct_answer;
+            fadeAnimation();
         } else {
             snackBarId = R.string.wrong_answer;
+            shakeAnimation();
         }
         Snackbar.make(binding.cardView, snackBarId, Snackbar.LENGTH_SHORT)
                 .show();
@@ -74,4 +79,56 @@ public class MainActivity extends AppCompatActivity {
         binding.questionTextView.setText(question);
     }
 
+    private void updateCounter() {
+        binding.questionIndexText.setText("Question: " + String.valueOf(currentQuestionNumber) + "/" + String.valueOf(questions.size()));
+    }
+
+    private void shakeAnimation(){
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,
+                R.anim.shake_animation);
+        binding.cardView.setAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        }
+
+    private void fadeAnimation(){
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+        alphaAnimation.setDuration(300);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        binding.cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
 }
